559ea5e7d398c3017ced7c91ce4195c7631deaaf
Eleyowo Michael <michael.eleyowo@bitpowr.com>
2022-12-28 18:04:54 +0100

    initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,39 @@
+import { CreateMessageDto } from './dtos/create-message.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // dont do this on real app
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get(':id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message Not found');
+    }
+    return message;
+  }
+}
